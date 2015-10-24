7b0bc57cde48bd0cbe561e8550375721d5464b26
gabigtr123 <bartok.gabi17@gmail.com>
2015-10-24 12:18:03 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var myList =['apples','oranges','bananas'];
+ myList[3] ='cirese';
+myList.forEach(function(value, index) {
+	alert(' I have '+value+' in  my shopping bag');
+});
+
+
+var myList =['Cartofi','Banane','Portocale'];
+for ( var i=0; i<myList.length; i++){
+	alert('Avem '+myList[i]+ ' in magazin ce bine'+' :) ')
+	console.log(myList[i]);
+}
